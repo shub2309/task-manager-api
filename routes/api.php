01f4b3aa4882a1bad3844270983fcbb694b957de
Shubham Jadhav <shubhamnj23@gmail.com>
2025-12-19 18:30:35 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,21 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('tasks', TaskController::class);
     Route::apiResource('users', \App\Http\Controllers\UserController::class);
 });
+
+Route::get('/test-db', function () {
+    try {
+        \DB::connection()->getPdo();
+        return response()->json(['status' => 'success', 'message' => 'Connected to ' . \DB::connection()->getDatabaseName()]);
+    } catch (\Exception $e) {
+        return response()->json(['status' => 'error', 'message' => $e->getMessage()], 500);
+    }
+});
+
+Route::get('/debug-env', function () {
+    return response()->json([
+        'app_env' => env('APP_ENV'),
+        'app_debug' => env('APP_DEBUG'),
+        'db_connection' => env('DB_CONNECTION'),
+        'db_host' => env('DB_HOST'),
+    ]);
+});
