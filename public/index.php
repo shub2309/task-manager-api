dece0193a7a8f4ca8b00c750513490f0ef3d1429
Shubham Jadhav <shubhamnj23@gmail.com>
2025-12-21 21:38:39 +0530

    refactor: Consolidate application entry point and CORS logic into `public/index.php` while deleting root `index.php` and `nixpacks.toml`.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// CORS manual fix - High Priority
+// CORS manual fix
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
@@ -9,13 +9,6 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    }
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    }
-    header("HTTP/1.1 204 No Content");
     exit;
 }
 
