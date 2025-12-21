3c5a0d73ba866eccd91d8a11a3e6ae4ce3149bc7
Shubham Jadhav <shubhamnj23@gmail.com>
2025-12-21 20:44:42 +0530

    feat: add initial application bootstrap, CORS configuration, and authentication exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
 
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->prepend(HandleCors::class);
         $middleware->trustProxies(at: '*');
     })
 
--- config/cors.php
+++ config/cors.php
@@ -2,20 +2,17 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://task-manager-ui-bay.vercel.app',
-        'http://localhost:5173',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
